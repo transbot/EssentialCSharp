3ea157d962b3c16c9d39c9a8fd827d9be54d8f26
Zhou Jing <transbot@gmail.com>
2024-02-07 01:38:20 +0800

    最后的修整，batch 1

--- src/Chapter18/Listing18.10.AssemblyAttributesWithinAssemblyInfo.cs
+++ src/Chapter18/Listing18.10.AssemblyAttributesWithinAssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 有关程序集的一般信息由以下
-// 控制。更改这些特性值可修改
-// 与程序集关联的信息。
+// 有关程序集的一般信息由以下特性
+// 控制。更改这些特性值可修改与
+// 程序集关联的信息。
 [assembly: AssemblyTitle("Bookzhou.com")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -15,11 +15,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
-//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
-//请将此类型的 ComVisible 特性设置为 true。
+// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+// 请将此类型的 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 
-// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+// 如果此项目向COM公开，则下列GUID用于类型库的ID
 [assembly: Guid("8709b811-b000-412c-a4b5-5021c64ae15d")]
 
 // 程序集的版本信息由下列四个值组成: 
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
-//通过使用 "*"，如下所示:
+// 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
+// 通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
